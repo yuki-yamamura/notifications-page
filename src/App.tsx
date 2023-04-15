0220e694a528aafc92cfde234fb82f125967338a
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-15 13:01:44 +0900

    complete creating Notification page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
+import useNotifications from 'hooks/useNotifications';
+import NotificationCard from 'components/NotificationCard';
 import { FC } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p className="underline">
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App: FC = () => {
+  const [notifications, updateNotifications] = useNotifications();
+  const sortedNotifications = notifications?.sort((a, b) =>
+    a.id > b.id ? -1 : 1,
+  );
+  const unreadNotifications = notifications?.filter(
+    (notification) => !notification.isRead,
+  );
+
+  const markAllNotificationsRead = () => {
+    // if all notifications has already been read, there's noting to do.
+    if (unreadNotifications === undefined || unreadNotifications.length === 0)
+      return;
+
+    void updateNotifications(
+      unreadNotifications.map((notification) => ({
+        ...notification,
+        isRead: true,
+      })),
+    );
+  };
+
+  return (
+    <div className="bg-white px-4 sm:px-5 md:rounded-2xl">
+      <header className="flex justify-between pb-6 pt-14">
+        <div className="flex gap-x-2">
+          <div className="text-xl font-bold text-veryDarkBlue">
+            Notifications
+          </div>
+          <div className="flex items-center rounded-md bg-blue px-3 py-[0.5] text-base font-bold text-white">
+            {unreadNotifications?.length ?? 0}
+          </div>
+        </div>
+        <button
+          type="button"
+          onClick={markAllNotificationsRead}
+          className="hover:text-blue focus:text-blue focus:outline-none"
+        >
+          Mark all as read
+        </button>
+      </header>
+      <main className="flex flex-col gap-y-3">
+        {sortedNotifications?.map((notification) => (
+          <NotificationCard notification={notification} />
+        ))}
+      </main>
+    </div>
+  );
+};
 
 export default App;
